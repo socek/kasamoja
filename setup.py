a16d3a9ce5e3a4c4b1d8bece58b588af12bde9e9
Dominik "Socek" Dlugajczyk <msocek@gmail.com>
2014-07-14 22:22:17 +0200

    Initial commit by PyProject.

---
+++ setup.py
@@ -0,0 +1,14 @@
+# -*- encoding: utf-8 -*-
+from setuptools import setup, find_packages
+
+install_requires = [
+]
+
+if __name__ == '__main__':
+    setup(name='KasaMoja',
+          version='0.1',
+          packages=find_packages('src'),
+          package_dir={'': 'src'},
+          install_requires=install_requires,
+          include_package_data=True,
+          )
